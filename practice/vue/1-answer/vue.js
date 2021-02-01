8848e1d90e71243bed2f4f938db1cf69cba51ac4
AndSmi <andsmidev@gmail.com>
2021-02-01 13:45:31 +0300

    Added answer code and fixed my code by review and answer

--- practice/vue/1-answer/vue.js
+++ practice/vue/1-answer/vue.js
@@ -1,11 +1,8 @@
-// вы можете как угодно изменять программу и код
-// добавлять любые переменные и модели
-// ваша задача реализовать так, как показано на видео, чтобы оно работало
-
 const App = {
   data() {
     return {
-      activeIndex: 0, // то, что позволяет определить текущий активный шаг
+      activeIndex: 0,
+      isActive: true,
       steps: [
         {title: 'Основы', text: 'В блоке вы познакомитесь со всеми основами Vue.js на практике. На протяжении блока мы напишем реактивное приложение, в процессе разработки которого разберем вся базу фреймворка.'},
         {title: 'Компоненты', text: 'Один из самых важных блоков в курсе, где вы узнаете все о компонентах. В блоке мы напишем 2 разных приложения и создадим более 5 различных UI компонентов как в реальной разработке. Блок расскажет про абсолютно все составляющие, которые есть в компонентах: взаимодействие, slots, асинхронные и динамические компоненты и тонна примеров.'},
@@ -17,39 +14,34 @@ const App = {
   },
   methods: {
     prev() {
-      // когда нажимаем кнопку назад
-      if(this.activeIndex !== 0) {
-        --this.activeIndex
+      if (this.activeIndex !== 0) {
+        this.activeIndex--
       }
     },
     reset() {
-      // начать заново
       this.activeIndex = 0
+      this.isActive = true
     },
     nextOfFinish() {
-      // кнопка вперед или закончить
-      if(this.activeIndex !== this.steps.length - 1) {
-        ++this.activeIndex
+      if (this.activeIndex !== this.steps.length - 1) {
+        this.activeIndex++
+      } else {
+        this.isActive = false
       }
     },
     setActive(idx) {
-      // когда нажимаем на определенный шаг
       this.activeIndex = idx
     }
   },
   computed: {
-    // тут стоит определить несколько свойств:
-    // 1. текущий выбранный шаг
-    currentStep() {
+    activeStep() {
       return this.steps[this.activeIndex]
     },
-    // 2. выключена ли кнопка назад
-    isDisabledPrev() {
-      return !this.activeIndex
+    prevDisabled() {
+      return this.activeIndex === 0
     },
-    // 3. находимся ли мы на последнем шаге
     isLastStep() {
-      return (this.activeIndex < this.steps.length - 1)
+      return this.activeIndex === this.steps.length - 1
     }
   }
 }
